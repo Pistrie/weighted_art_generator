1b5086e46fcd46772fc9b63a28f62802c81b5ea0
Sylvester Roos <sylvester.roos@xs4all.nl>
2023-02-13 11:22:07 +0100

    initial commit

---
+++ lib/weighted_art_generator.ex
@@ -0,0 +1,18 @@
+defmodule WeightedArtGenerator do
+  @moduledoc """
+  Documentation for `WeightedArtGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WeightedArtGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/weighted_art_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule WeightedArtGeneratorTest do
+  use ExUnit.Case
+  doctest WeightedArtGenerator
+
+  test "greets the world" do
+    assert WeightedArtGenerator.hello() == :world
+  end
+end
