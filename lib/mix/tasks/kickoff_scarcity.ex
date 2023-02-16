6f3169c733d4432d89661aad317701c3537c36fb
Sylvester Roos <sylvester.roos@xs4all.nl>
2023-02-16 13:52:32 +0100

    added default parameter

--- lib/mix/tasks/kickoff_scarcity.ex
+++ lib/mix/tasks/kickoff_scarcity.ex
@@ -3,16 +3,17 @@ defmodule Mix.Tasks.Kickoff.Scarcity do
 
   @shortdoc "Creates amount of images equal to 1/3 of the possible combinations"
   @moduledoc """
-  Creates the amount of images equal to 1/3 of the possible combinations
+  Creates the amount of images equal to 1/3 of the possible combinations. You can supply it with a parameter for the amount of processes that will be used to generate the images
 
   ## Examples
 
   ```
   mix kickoff.scarcity
+  mix kickoff.scarcity 400
   ```
   """
 
-  def run([n_workers | _]) do
+  def run([n_workers | _] \\ ["100"]) do
     {n_workers, ""} = Integer.parse(n_workers, 10)
     WeightedArtGenerator.kickoff_scarcity(n_workers)
   end
